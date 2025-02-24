9861110dcfc882f0b252b792010e196a77c70f98
khalidesskali <esskalikhalid7@gmail.com>
2025-02-24 11:02:51 +0000

    add Reveal scroll animation component

---
+++ Client/src/components/Reveal.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+
+const Reveal = ({ children }) => {
+  const controls = useAnimation();
+  const ref = useRef();
+
+  const inView = useInView(ref, { once: true, amount: 0.1 });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  return (
+    <motion.div
+      ref={ref}
+      variants={{
+        hidden: { opacity: 0, y: 75 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      initial="hidden"
+      animate={controls}
+      transition={{ duration: 0.5, delay: 0.25 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default Reveal;
