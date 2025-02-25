cb09084cc77d8cf89952b50126298d7eec7f52bd
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-02-25 12:26:45 +0000

    authentication phase

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -2,31 +2,21 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
-
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 
-
-class AppServiceProvider extends ServiceProvider
+class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    // ...existing code...
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+
+        // ...existing code...
     }
 }
