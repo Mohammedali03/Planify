8bc0cff4e602cc3b7d4a8f83c9856efda5fd7e1a
khalidesskali <esskalikhalid7@gmail.com>
2025-03-08 17:45:53 +0000

    set up static signup and login forms

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
