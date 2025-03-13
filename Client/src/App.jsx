6b1c38fafcc7f58e01831db09f9b70c4cc67d0f5
khalidesskali <esskalikhalid7@gmail.com>
2025-03-13 15:00:28 +0000

    Add Routes and protect them

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,9 +7,14 @@ import Login from "./components/Login";
 import Layout from "./components/Layout";
 import Dashboard from "./components/Dashboard";
 import { useAuth } from "./components/AuthProvider";
+import StudyRoom from "./components/StudyRoom";
+import Leaderboard from "./components/Leaderboard";
+import StudyGoals from "./components/StudyGoals";
+import Settings from "./components/Settings";
 
 const App = () => {
   const { isAuthenticated } = useAuth();
+
   return (
     <Routes>
       <Route
@@ -29,7 +34,23 @@ const App = () => {
           path="/dashboard"
           element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
         />
+        <Route
+          path="/study-room"
+          element={isAuthenticated ? <StudyRoom /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/study-goals"
+          element={isAuthenticated ? <StudyGoals /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/leaderboard"
+          element={isAuthenticated ? <Leaderboard /> : <Navigate to="/login" />}
+        />
       </Route>
+      <Route
+        path="settings"
+        element={isAuthenticated ? <Settings /> : <Navigate to="/login" />}
+      />
     </Routes>
   );
 };
