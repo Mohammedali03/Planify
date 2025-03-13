5f02ae87e48703441dac7a69a1e2106880e5bb84
khalidesskali <esskalikhalid7@gmail.com>
2025-03-13 11:03:45 +0000

    protect Routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import "./index.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Layout from "./components/Layout";
 import Dashboard from "./components/Dashboard";
+import { useAuth } from "./components/AuthProvider";
 
 const App = () => {
+  const { isAuthenticated } = useAuth();
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={!isAuthenticated ? <Home /> : <Navigate to="/dashboard" />}
+      />
+      <Route
+        path="/signup"
+        element={!isAuthenticated ? <Signup /> : <Navigate to="/dashboard" />}
+      />
+      <Route
+        path="/login"
+        element={!isAuthenticated ? <Login /> : <Navigate to="/dashboard" />}
+      />
       <Route element={<Layout />}>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />}
+        />
       </Route>
     </Routes>
   );
