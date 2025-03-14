1864c60b7d23fcf7691e667488948ebd55957418
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-03-14 15:45:48 +0000

    adding  background table

--- server/app/Models/Background.php
+++ server/app/Models/Background.php
@@ -7,9 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Background extends Model
 {
+       
     use HasFactory;
 
     protected $fillable = ['user_id', 'name', 'image_path'];
+    // public function user()
+    // {
+    //     return $this->belongsTo(User::class);
+    // }
 
     public function user()
     {
