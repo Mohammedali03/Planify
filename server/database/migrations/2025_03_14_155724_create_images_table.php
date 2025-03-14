575d4ae8002ccca05be2f7cd26bed06da822ea25
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-03-14 15:57:48 +0000

    image model

--- server/database/migrations/2025_03_14_155724_create_images_table.php
+++ server/database/migrations/2025_03_14_155724_create_images_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('backgrounds', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Background belongs to a user
-            $table->string('name');
-            $table->string('image_path');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('backgrounds');
+        Schema::dropIfExists('images');
     }
 };
