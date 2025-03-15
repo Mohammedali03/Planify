ab8265d5af57f12e962052ce7f30f0c79be03204
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-03-15 10:38:12 +0000

    todo file

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\StudySessionController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -11,10 +12,16 @@ Route::get('/user', [AuthController::class, 'user']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/rooms', [RoomController::class, 'index']);
-Route::post('/rooms', [RoomController::class, 'store']);
-Route::post('/rooms/{room}/start', [RoomController::class, 'startSession']);
-Route::post('/rooms/{room}/pause', [RoomController::class, 'pauseSession']);
-Route::post('/rooms/{room}/end', [RoomController::class, 'endSession']);
+// Route::post('/rooms', [RoomController::class, 'store']);
+// Route::post('/rooms/{room}/start', [RoomController::class, 'startSession']);
+// Route::post('/rooms/{room}/pause', [RoomController::class, 'pauseSession']);
+// Route::post('/rooms/{room}/end', [RoomController::class, 'endSession']);
+
+
+Route::post('/sessions/{id}/start', [StudySessionController::class, 'start']);
+Route::post('/sessions/{id}/pause', [StudySessionController::class, 'pause']);
+Route::post('/sessions/{id}/resume', [StudySessionController::class, 'resume']);
+Route::post('/sessions/{id}/end', [StudySessionController::class, 'end']);
 });
 
 
