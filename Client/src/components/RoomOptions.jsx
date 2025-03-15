0f8b1d464e12be20cf6f0cc6a810f3950e54fe48
khalidesskali <esskalikhalid7@gmail.com>
2025-03-15 17:28:26 +0000

    Start Study room and add options

---
+++ Client/src/components/RoomOptions.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const RoomOptions = ({
+  isFull,
+  handleBackgrounds,
+  handleSounds,
+  toggleFullScreen,
+}) => {
+  const options = [
+    {
+      name: "music",
+      svg: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+          />
+        </svg>
+      ),
+      click: handleSounds,
+    },
+    {
+      name: "background",
+      svg: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"
+          />
+        </svg>
+      ),
+      click: handleBackgrounds,
+    },
+    {
+      name: "fullScreen",
+      svg: isFull ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="size-6"
+        >
+          <path d="M3.28 2.22a.75.75 0 0 0-1.06 1.06L5.44 6.5H2.75a.75.75 0 0 0 0 1.5h4.5A.75.75 0 0 0 8 7.25v-4.5a.75.75 0 0 0-1.5 0v2.69L3.28 2.22ZM13.5 2.75a.75.75 0 0 0-1.5 0v4.5c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-2.69l3.22-3.22a.75.75 0 0 0-1.06-1.06L13.5 5.44V2.75ZM3.28 17.78l3.22-3.22v2.69a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.69l-3.22 3.22a.75.75 0 1 0 1.06 1.06ZM13.5 14.56l3.22 3.22a.75.75 0 1 0 1.06-1.06l-3.22-3.22h2.69a.75.75 0 0 0 0-1.5h-4.5a.75.75 0 0 0-.75.75v4.5a.75.75 0 0 0 1.5 0v-2.69Z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className="size-6"
+        >
+          <path d="m13.28 7.78 3.22-3.22v2.69a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.69l-3.22 3.22a.75.75 0 0 0 1.06 1.06ZM2 17.25v-4.5a.75.75 0 0 1 1.5 0v2.69l3.22-3.22a.75.75 0 0 1 1.06 1.06L4.56 16.5h2.69a.75.75 0 0 1 0 1.5h-4.5a.747.747 0 0 1-.75-.75ZM12.22 13.28l3.22 3.22h-2.69a.75.75 0 0 0 0 1.5h4.5a.747.747 0 0 0 .75-.75v-4.5a.75.75 0 0 0-1.5 0v2.69l-3.22-3.22a.75.75 0 1 0-1.06 1.06ZM3.5 4.56l3.22 3.22a.75.75 0 0 0 1.06-1.06L4.56 3.5h2.69a.75.75 0 0 0 0-1.5h-4.5a.75.75 0 0 0-.75.75v4.5a.75.75 0 0 0 1.5 0V4.56Z" />
+        </svg>
+      ),
+      click: toggleFullScreen,
+    },
+  ];
+  return (
+    <ul className="options flex items-center bg-white gap-4 p-2 rounded text-black absolute right-5 top-3">
+      {options.map((option) => (
+        <li
+          key={option.name}
+          className="p-2 hover:cursor-pointer hover:bg-indigo-200 hover:text-indigo-600 rounded duration-300"
+          onClick={option.click}
+        >
+          <span>{option.svg}</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default RoomOptions;
--- Client/src/components/StudyRoom.jsx
+++ Client/src/components/StudyRoom.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import RoomOptions from "./RoomOptions";
 
 const StudyRoom = () => {
-  return <div>StudyRoom</div>;
+  const ref = useRef(null);
+  const [isFull, setIsFull] = useState(false);
+
+  function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+      ref.current?.requestFullscreen();
+      setIsFull(true);
+    } else {
+      document.exitFullscreen();
+    }
+  }
+
+  useEffect(() => {
+    function handleFullScreen() {
+      setIsFull(!!document.fullscreenElement);
+    }
+
+    document.addEventListener("fullscreenchange", handleFullScreen);
+
+    return () => removeEventListener("fullscreenchange", handleFullScreen);
+  }, []);
+
+  function handleBackgrounds() {
+    alert("not created yet");
+  }
+  function handleSounds() {
+    alert("not created yet");
+  }
+
+  return (
+    <div
+      ref={ref}
+      className="study-room relative bg-cover bg-center h-screen bg-[url('./images/pexels-juanpphotoandvideo-877971.jpg')]"
+    >
+      <div className="absolute w-full h-full bg-black opacity-30"></div>
+      <RoomOptions
+        isFull={isFull}
+        toggleFullScreen={toggleFullScreen}
+        handleBackgrounds={handleBackgrounds}
+        handleSounds={handleSounds}
+      />
+    </div>
+  );
 };
 
 export default StudyRoom;
