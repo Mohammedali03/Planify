905769e238c51cc04cd8743f6c41beb9b3e43b4c
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-03-24 16:51:40 +0000

    feat: add user credentials, email, and password update methods in ProfileController

--- server/app/Http/Controllers/User/ProfileController.php
+++ server/app/Http/Controllers/User/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -20,10 +21,47 @@ class ProfileController extends Controller
 
     }
 
-    public function pic(){
+    // public function pic(){
+    //     $user=auth()->user();
+    //     $profile_picture=$user->profile_picture_url;
+    //     return response()->json(['profile_picture'=>$profile_picture]);
+    //     // return response()->json(['profile_picture'=>$user->profile_picture]);
+    // }
+
+        public function credentials(Request $request){
+       $validated = $request->validate([
+            'firstName'=>'required|string|max:255',
+            'lastName'=>'required|string|max:255',
+       ]);
+
+       $user = auth()->user();
+       $name = $validated['firstName'] . ' ' . $validated['lastName'];
+
+       $user->name = $name;
+       $user->save();
+         return response()->json(['message'=>'Credentials Updated Successfully'],201);
+
+
+        }
+    public function update_email(Request $request){
+        $request->validate([
+            'email'=>'required|email|unique:users'
+        ]);
+
         $user=auth()->user();
-        $profile_picture=$user->profile_picture_url;
-        return response()->json(['profile_picture'=>$profile_picture]);
-        // return response()->json(['profile_picture'=>$user->profile_picture]);
+        $user->email=$request->email;
+        $user->save();
+        return response()->json(['message'=>'Email Updated Successfully'],201);
+    }
+
+    public function update_password(Request $request){
+        $request->validate([
+            'password'=>'required|string|min:8'
+        ]);
+
+        $user=auth()->user();
+        $user->password=bcrypt($request->password);
+        $user->save();
+        return response()->json(['message'=>'Password Updated Successfully'],201);
     }
 }
