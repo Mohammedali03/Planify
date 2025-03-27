e75b9863b7e10cd5a533dcfe11511ec403c603cf
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-03-27 13:36:05 +0000

    feat: add CORS middleware to handle cross-origin requests

---
+++ server/app/Http/Middleware/HandleCors.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\HandleCors as Middleware;
+
+class HandleCors extends Middleware
+{
+    protected $headers = [
+        'Access-Control-Allow-Origin' => 'http://localhost:5173', // Change this to match your frontend
+        'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials' => 'true',
+    ];
+}
+
+
--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use App\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,12 +15,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // $middleware->group('api', [
-        //     EnsureFrontendRequestsAreStateful::class,
-        //     'throttle:api',
-        //     SubstituteBindings::class,
-        // ]);
-        
+        // Global middleware
+        $middleware->append(HandleCors::class);
+
+        // API middleware group
+        $middleware->group('api', [
+            EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            SubstituteBindings::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
