a1ae3680319432e8f2f631cdd5de8f0b565aed94
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-04-19 19:47:46 +0100

    Add methods to StatsController for user streak, max streak, and last visit retrieval

--- server/app/Http/Controllers/Stats/StatsController.php
+++ server/app/Http/Controllers/Stats/StatsController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-   public function 
+   public function streak(){
+      $user = auth()->user();
+      $streak = $user->streak;
+      return response()->json([
+         'streak' => $streak
+      ]);
+   }
+
+   public function max_streak(){
+      $user = auth()->user();
+      $max_streak = $user->max_streak;
+      return response()->json([
+         'max_streak' => $max_streak
+      ]);
+
+   }
+
+   public function last_visit(){
+      $user = auth()->user();
+      $last_visit = $user->last_visit_at;
+      return response()->json([
+         'last_visit' => $last_visit
+      ]);
+   }
 }
