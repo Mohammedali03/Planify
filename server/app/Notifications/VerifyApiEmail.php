dc6d55d5bac26185aeebc11631b72f028158a312
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-04-22 15:55:43 +0100

    Add VerifyApiEmail notification class for customized email verification. This class extends the base verification functionality to generate a temporary signed URL for email verification, enhancing user account security and management.

---
+++ server/app/Notifications/VerifyApiEmail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use Illuminate\Auth\Notifications\VerifyEmail as BaseVerifyEmail;
+use Illuminate\Support\Facades\URL;
+
+class VerifyApiEmail extends BaseVerifyEmail
+{
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            now()->addMinutes(60),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+    }
+}
