ab2cfe08a9a7ab75b4a7388189f264cf9443652f
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-04-24 16:06:42 +0100

    Update console routes to schedule inactivity email command every minute. This change enhances the scheduling functionality for user engagement reminders.

--- server/routes/console.php
+++ server/routes/console.php
@@ -3,8 +3,16 @@
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+use Illuminate\Support\Facades\Schedule;
+use App\Console\Commands\SendInactivityEmail;
 Artisan::command('inspire', function () {
     /** @var ClosureCommand $this */
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+
+// Schedule::command(SendInactivityEmail::class)->dailyAt('10:00');
+Schedule::command(SendInactivityEmail::class)->everyMinute();
+
+
