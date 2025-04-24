f8e209c7f5c63506f29fcc101474159bb14f7b36
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-04-24 16:06:32 +0100

    Add test email route to send inactivity reminders. This introduces a new endpoint for testing email functionality using the InactivityReminder mail class.

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Mail\InactivityReminder;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-email', function () {
+    Mail::to('test@example.com')->send(new InactivityReminder(User::first()));
 });
