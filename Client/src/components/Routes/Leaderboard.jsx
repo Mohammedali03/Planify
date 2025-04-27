6fefbc83492e4c35b13787431663921111e2d8f7
khalidesskali <esskalikhalid7@gmail.com>
2025-04-27 17:56:42 +0100

    remove unused import

--- Client/src/components/Routes/Leaderboard.jsx
+++ Client/src/components/Routes/Leaderboard.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import axios from "axios";
-import { useFetch } from "../CustomHooks/useFetch";
 
 const Leaderboard = () => {
   const [students, setStudents] = useState([]);
