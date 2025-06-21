de06a78ef11bb3808d9e645a7fb39ea6eea34837
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2025-06-21 18:36:35 +0100

    fix: update CORS headers to include X-XSRF-TOKEN for enhanced security in API requests

--- server/app/Http/Middleware/HandleCors.php
+++ server/app/Http/Middleware/HandleCors.php
@@ -8,7 +8,7 @@ class HandleCors extends Middleware
     protected $headers = [
         'Access-Control-Allow-Origin' => 'http://localhost:5173', // Change this to match your frontend
         'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers' => 'Content-Type, Authorization',
+        'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-XSRF-TOKEN',
         'Access-Control-Allow-Credentials' => 'true',
     ];
 }
